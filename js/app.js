e88c63787e4086a5288ac1b4e7162dc31ae59d28
Facundo Brochero <facu19881@gmail.com>
2023-05-03 23:11:01 -0300

    haciendo cosas en javascript

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ function login() {
 
     if (user == "Facundo" && pass == "1234" || user == "Profesor" && pass == 1234) {
         window.location = "../pages/bienvenida.html"
+    } else {
+        alert("Algun dato esta Incorrecto")
+        window.location = "../inicio.html"
     }
 }
 
@@ -19,3 +22,32 @@ document.addEventListener("keyup", (e) => {
     }
 })
 
+class curso {
+    constructor(titulo, precio) {
+        this.titulo = titulo;
+        this.precio = precio;
+        this.gracias = function () { console.log("Gracias, por interesarte en el " + this.titulo) }
+    }
+}
+
+const curso1 = new curso("Curso de Introducción a las Finanzas", 35000);
+const curso2 = new curso("Curso de Criptomonedas: Inversiones y Finanzas. Bitcoins y Tecnología Blockchain", 65000);
+const curso3 = new curso("Curso de Cómo invertir y operar en la bolsa argentina online - Programa Integral", 65000);
+
+curso1.gracias();
+curso2.gracias();
+curso3.gracias();
+
+const cursos = [curso1, curso2, curso3]
+
+for (let i = 0; i < cursos.length; i++) {
+    console.log(cursos[i])
+}
+
+console.log(cursos.length)
+
+cursos.push("nuevo Curso al Final")
+console.log(cursos)
+
+cursos.unshift("nuevo Curso al inicio")
+console.log(cursos)
